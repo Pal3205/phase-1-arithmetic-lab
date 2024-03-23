69937402c1fd7a51350f358da9a4073702e0fb14
Pebble <pal19carson.m@gmai.com>
2024-03-23 04:38:49 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+/*is an equation whose multiplied results will equal 62*/
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2;
 
-// Write your code here
+/*generates a random integer greater than 0*/
+console.log(multiply)
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+console.log(random);  
+
+/*is an equation that calculates a remainder that is equal to 4*/
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+console.log(mod);
+/*will return 20 as the highest number in the set*/
+let num5 = 16;
+let num6 = 18;
+let num7 = 9;
+let num8 = 7;
+let num9 = 5;
+let num10 = 1;
+let num11 = 15;
+let num12 = 20;
+
+let max = Math.max(num5,num6,num7,num8,num9,num10,num11,num12);
+
+console.log(max);
